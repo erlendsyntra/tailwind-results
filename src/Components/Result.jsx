1779c3698918595c51f5bfefd5febb3c2833f77a
erlendsyntra <erlendgeerts@gmail.com>
2024-01-24 10:33:34 +0100

    desktop version pixel perfect

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,7 +1,6 @@
 const Result = () => {
   return (
     <>
-      {/* bg-gradient-to-b from-pro-light-slate-blue to-pro-light-royal-blue */}
       <div className="bg-gradient-to-b from-pro-light-slate-blue to-pro-light-royal-blue text-center rounded-3xl md:w-1/2 h-128 shadow-2xl">
         <p className="font-medium text-pro-light-lavender text-2xl pt-9">
           Your Result
@@ -10,8 +9,8 @@ const Result = () => {
           <p className="text-7xl font-extrabold text-pro-white pt-14">76</p>
           <p className="text-pro-pale-blue">of 100</p>
         </div>
-        <p className="text-pro-white md:text-2xl font-bold md:pt-8">Great</p>
-        <p className="text-pro-pale-blue/90 m-auto w-64 md:pt-6 md:leading-snug">
+        <p className="text-pro-white md:text-3xl font-bold md:pt-6">Great</p>
+        <p className="text-pro-pale-blue/90 m-auto w-64 md:pt-3 md:leading-snug">
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -29,9 +29,10 @@ const Summary = () => {
       <div className="bg-pro-white rounded-rb-3xl md:w-1/2 h-128 shadow-2xl rounded-3xl px-10">
         <h1 className="font-medium text-2xl pt-9">Summary</h1>
         <ul className="md:text-xl space-y-5 pt-8 font-extrabold">
-          {results.map(({ text, liColor, textColor, score }) => {
+          {results.map(({ id, text, liColor, textColor, score }) => {
             return (
               <SummaryItem
+                key={id}
                 text={text}
                 liColor={liColor}
                 textColor={textColor}
@@ -39,34 +40,12 @@ const Summary = () => {
               />
             );
           })}
-          {/* <SummaryItem
-            text="Reaction"
-            liColor="bg-pro-light-red/10"
-            textColor="text-pro-light-red"
-            score="80"
-          />
-          <SummaryItem
-            text="Memory"
-            liColor="bg-pro-orange-yellow/10"
-            textColor="text-pro-orange-yellow"
-            score="92"
-          />
-          <SummaryItem
-            text="Verbal"
-            liColor="bg-pro-green-teal/10"
-            textColor="text-pro-green-teal"
-            score="61"
-          />
-          <SummaryItem
-            text="Visual"
-            liColor="bg-pro-cobalt-blue/10"
-            textColor="text-pro-cobalt-blue"
-            score="72"
-          /> */}
         </ul>
         <a
           href="#"
-          className="bg-pro-dark-grayblue text-pro-white text-center md:mt-10 text-xl font-extrabold w-full py-4 inline-block rounded-full"
+          className="bg-pro-dark-grayblue text-pro-white text-center md:mt-10 text-xl 
+          font-extrabold w-full py-4 inline-block rounded-full hover:text-pro-pale-blue/60 
+          hover:hover:bg-gradient-to-b from-pro-light-slate-blue to-pro-light-royal-blue transition-all"
         >
           Continue
         </a>
